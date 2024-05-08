1018f3ef76fc8b4fce9db0e822a214ec0f6d9eaf
Mayank <palmayank2100@gmail.com>
2024-05-08 14:05:12 +0530

    Add attacking to enemy

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,21 +1,25 @@
 extends Area2D
 
 
-const MAX_HEALTH: float = 10.0
+@onready var player: CharacterBody2D = $'../Player'
+
+
+const MAX_HEALTH: float = 30.0
 var health: float
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	health = MAX_HEALTH
+	player.connect('DamageDealt', Callable(self, 'take_damage'))
 	
 
 func take_damage(damage: float):
 	health -= damage
-	
+	print(health)
 	if health <= 0:
-		get_parent().queue_free()
+		queue_free()
 
 
-func _on_area_entered(characterBody2d: CharacterBody2D) -> void:
-	take_damage(5)
+func _on_body_entered(body: Node2D) -> void:
+	take_damage(10.0)
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,17 +1,22 @@
 extends CharacterBody2D
 
 
+signal DamageDealt(damage)
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var sword_hitbox_left: CollisionShape2D = $SwordHitboxLeft
+@onready var sword_hitbox_right: CollisionShape2D = $SwordHitboxRight
+@onready var timer: Timer = $Timer
 
 var rng = RandomNumberGenerator.new()
 var speed = 150
 var animation_lock = false
+var damage: float = 10.0;
+
 
 func _physics_process(delta):
-	
+	# Movement
 	var direction_x = Input.get_axis("left", "right")
 	var direction_y = Input.get_axis("up", "down")
-	
 	
 	if direction_x && !animation_lock:
 		velocity.x = direction_x * speed * delta
@@ -23,36 +28,53 @@ func _physics_process(delta):
 	else:
 		velocity.y = move_toward(velocity.y, 0, speed)
 
-	
+	# Movement animation
 	if direction_x == 0 && direction_y == 0:
 		_change_animation("idle")
-		#animated_sprite_2d.play("idle")
 	else:
 		_change_animation("run")
-		#animated_sprite_2d.play("run")
 		if direction_x < 0:
 			animated_sprite_2d.flip_h = true
-		elif direction_x > 0: animated_sprite_2d.flip_h = false
+
+		elif direction_x > 0:
+			animated_sprite_2d.flip_h = false
 
 	velocity = velocity.normalized() * speed
 	move_and_slide()
 
-	if (Input.is_action_just_pressed("attack")):	
+
+	# Attack Animation
+	if (Input.is_action_just_pressed("attack")):
 		var attack_number = rng.randi_range(0, 1)
+
 		if attack_number == 0:
 			_change_animation("attack1")
-			#animated_sprite_2d.play("attack1")
 			animation_lock = true
 		elif attack_number == 1:
 			_change_animation("attack2")
-			#animated_sprite_2d.play("attack2")
 			animation_lock = true
 		else:
 			print(attack_number)
 
+		timer.start(0.1)
+		if timer.time_left >= 0.0:
+			if animated_sprite_2d.flip_h:
+				sword_hitbox_left.disabled = false
+			else:
+				sword_hitbox_right.disabled = false
+
+
 func _change_animation(animation_name):
 	if animation_lock == false:
 		animated_sprite_2d.play(animation_name)
+		sword_hitbox_left.disabled = true
+		sword_hitbox_right.disabled = true
+
 
 func _on_animation_finished():
 	animation_lock = false
+
+
+func deal_damage():
+	emit_signal('Damage', damage)
+
